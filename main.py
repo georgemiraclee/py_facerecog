5e4b04bc4e63457c523444917ffe4df243659d62
georgemiraclee <georgemiracle2019@gmail.com>
2024-03-03 00:09:29 +0700

    fix error

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import cv2
 
-face_ref =cv2.CascadeClassifier("face_ref.xml")
+face_ref = cv2.CascadeClassifier("face_ref.xml")
 camera = cv2.VideoCapture(0)
 
 def face_detection(frame):
-    optimized_frame =cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    faces = face_ref.detectMultiScale(frame, scaleFactor = 1.1)
+    optimized_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
+    faces = face_ref.detectMultiScale(optimized_frame, scaleFactor=1.1)
     return faces
 
 def drawer_box(frame):
     for x, y, w, h in face_detection(frame):
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 4)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 4)
 
 def close_window():
     camera.release()
@@ -19,11 +19,11 @@ def close_window():
 
 def main():
     while True:
-        frame = camera.read()
+        _, frame = camera.read()  
         drawer_box(frame)
         cv2.imshow("AI FACE", frame)
         
-        if cv2.waitKey(1) & 0xFF == ord ('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             close_window()
 
 if __name__ == '__main__':
