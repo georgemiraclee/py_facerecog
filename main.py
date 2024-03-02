4e4ff42f9716e6eafc78d50dc8dbf5ef758575b9
georgemiraclee <georgemiracle2019@gmail.com>
2024-03-03 00:12:37 +0700

    bagusin dikit

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ face_ref = cv2.CascadeClassifier("face_ref.xml")
 camera = cv2.VideoCapture(0)
 
 def face_detection(frame):
-    optimized_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
-    faces = face_ref.detectMultiScale(optimized_frame, scaleFactor=1.1)
+    optimized_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_ref.detectMultiScale(optimized_frame, scaleFactor=1.1, minSize=(100, 100), minNeighbors=5)
     return faces
 
 def drawer_box(frame):
     for x, y, w, h in face_detection(frame):
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 4)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 4)
 
 def close_window():
     camera.release()
@@ -19,7 +19,11 @@ def close_window():
 
 def main():
     while True:
-        _, frame = camera.read()  
+        ret, frame = camera.read()
+        if not ret:
+            print("Error reading frame")
+            break
+        
         drawer_box(frame)
         cv2.imshow("AI FACE", frame)
         
